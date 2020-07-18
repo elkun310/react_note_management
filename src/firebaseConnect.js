ef2382fedeb4b69a882ad75558fab2866b34dad7
HaNQ <nguyenha9772@gmail.com>
2020-07-18 23:33:41 +0700

    create firebaseConnect js file

---
+++ src/firebaseConnect.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyABX1kTHUjl7-usfiKM4Rp8YKpk9gsGt-0",
+    authDomain: "react-note-management-9c733.firebaseapp.com",
+    databaseURL: "https://react-note-management-9c733.firebaseio.com",
+    projectId: "react-note-management-9c733",
+    storageBucket: "react-note-management-9c733.appspot.com",
+    messagingSenderId: "146110528765",
+    appId: "1:146110528765:web:e660b02fa212f7b3bb5294",
+    measurementId: "G-E1THBX79DV"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
